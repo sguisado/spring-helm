4f3d7f0dc918fcbe209a6106a836b35ccc2a52d5
Sergio Guisado <sr.guisado@gmail.com>
2021-04-09 12:12:46 +0200

    first commit

---
+++ src/main/java/com/clearpay/springhelm/SpringHelmApplication.java
@@ -0,0 +1,13 @@
+package com.clearpay.springhelm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelmApplication.class, args);
+	}
+
+}
